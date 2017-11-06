1f7149d23b6759d6f434748cccd82d460a111f9c
Vinicius Sarmento <vinicius.eng.comp92@gmail.com>
2017-11-06 20:42:57 -0200

    Alteração

--- TaxiPetP/src/pages/clientecadastro/clientecadastro.ts
+++ TaxiPetP/src/pages/clientecadastro/clientecadastro.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-clientecadastro',
   templateUrl: 'clientecadastro.html'
 })
+
 export class ClienteCadastroPage {
 
 }
