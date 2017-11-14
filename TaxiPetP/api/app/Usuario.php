649de926eeaa2aa66d99cca4966a53f3cc02b49b
José Elias Fernandes <zeeliasf@gmail.com>
2017-11-14 19:37:35 -0200

    .

--- TaxiPetP/api/app/Usuario.php
+++ TaxiPetP/api/app/Usuario.php
@@ -8,5 +8,5 @@ class Usuario extends Model
 {
     protected $table = "usuario";
 
-    protected $fillable = ['id', 'nome', 'email', 'senha', 'cpf', 'rg', 'telefone', 'endereco', 'cnh', 'is_admin'];
+    protected $fillable = ['nome', 'email', 'senha', 'cpf', 'rg', 'telefone', 'endereco', 'cnh', 'is_admin'];
 }
